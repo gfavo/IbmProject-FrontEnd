f5c27e10aaeadd88c1077dc49c1dddb73e36cc55
gfavo <gabrielecientista@gmail.com>
2023-07-13 09:02:33 -0300

    Primeiro commit frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdditionalModeComponent } from './additional-mode/additional-mode.component';
+import { LegacyModeComponent } from './legacy-mode/legacy-mode.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'additional', component:AdditionalModeComponent},
+  {path:'', component:LegacyModeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Candidato } from 'src/models/candidato';
+import { Status } from 'src/models/status';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'my-app';
